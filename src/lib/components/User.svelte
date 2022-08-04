92eb621f3ec46e6b6951fd94f52529c607ca311e
Jana Rajakumar <jana.rajakumar@utoronto.ca>
2022-08-04 02:03:17 -0400

    WIP: Setup initial card positions

--- src/lib/components/User.svelte
+++ src/lib/components/User.svelte
@@ -8,9 +8,9 @@
 
 <div
 	on:click={onUserClick}
-	class=" absolute h-[200px] w-[150px] cursor-pointer rounded-lg border border-gray-200 shadow-sm"
-	style="left: {user.position.x}px;
-    top: {user.position.y}px; "
+	class="absolute h-[200px] w-[150px] cursor-pointer rounded-lg border border-gray-200 shadow-sm"
+	style="left: {user.x}px;
+    top: {user.y}px; "
 >
 	<h1>{user.name}</h1>
 </div>
--- src/lib/store.js
+++ src/lib/store.js
@@ -2,29 +2,62 @@ import { writable } from 'svelte/store'
 
 export const d3Scale = writable(1)
 
-export const users = writable([
-	{
-		id: 1,
-		name: 'John Doe',
-		position: {
-			x: 50,
-			y: 50
-		}
-	},
-	{
-		id: 2,
-		name: 'Jane Doe',
-		position: {
-			x: 300,
-			y: 100
-		}
-	},
-	{
-		id: 3,
-		name: 'Jack Doe',
-		position: {
-			x: 100,
-			y: 350
-		}
+const innerWidth = 900
+const innerHeight = 600
+const userCardSize = {
+	width: 150,
+	height: 200
+}
+
+const setInitialPositions = (users) => {
+	// set initial positions of users on the screen starting from the center and building in top right bottom left order with a gap of 50px between each user
+	const center = {
+		x: innerWidth / 2,
+		y: innerHeight / 2
 	}
-])
+	const gap = 50
+	const positionedUsers = []
+	// place users starting from center and building in top right bottom left order with a gap of 50px between each user
+	for (let i = 0; i < users.length; i++) {
+		const user = users[i]
+		const position = {
+			x: center.x,
+			y: center.y
+		}
+		if (i % 2 === 0) {
+			position.x -= (userCardSize.width + gap) * (i / 2)
+		}
+		if (i % 2 === 1) {
+			position.x += (userCardSize.width + gap) * (i / 2)
+		}
+		if (i <= 2) {
+			position.y -= (userCardSize.height + gap) * (i / 2)
+		}
+		if (i > 2) {
+			position.y += (userCardSize.height + gap) * (i / 2)
+		}
+		positionedUsers.push({
+			...user,
+			...position
+		})
+	}
+	console.log(positionedUsers)
+	return positionedUsers
+}
+
+const getUserData = () => {
+	// generate random 100 users with random names and random age
+	const users = []
+	for (let i = 0; i < 6; i++) {
+		const name = `User ${i}`
+		const age = Math.floor(Math.random() * 100)
+		users.push({
+			id: i,
+			name,
+			age
+		})
+	}
+	return users
+}
+
+export const users = writable(setInitialPositions(getUserData()))
