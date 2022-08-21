a003236f8e52269a84b6dc85bb69bb6e2d482939
Arun Mathiyalakan <friday99@outlook.it>
2022-08-21 21:03:18 +0200

    ADD: all colorSchemes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,8 +6,18 @@ const config = {
 	theme: {
 		extend: {
 			colors: {
-        'default-primary': '#964CFF',
-        'default-secondary': '#4433FF'
+        'default-banner-primary': '#964CFF',
+        'default-banner-secondary': '#4433FF',
+				'inactive-banner-primary': '#A6A6A6',
+        'inactive-banner-secondary': '#999999',
+				'partner-banner-primary': '#FF3399',
+        'partner-banner-secondary': '#FF4C4D',
+				'parent-banner-primary': '#D5FF00',
+        'parent-banner-secondary': '#00CC21',
+				'child-banner-primary': '#FA00FF',
+        'child-banner-secondary': '#9E00E8',
+				'sibling-banner-primary': '#FF5C00',
+        'sibling-banner-secondary': '#FAFF00'
       },
 			fontSize: {
 				title:  ['12px', '14px'],
