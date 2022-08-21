9c7fda1282126b30c28dfcb4bd52226f19e5e623
Arun Mathiyalakan <friday99@outlook.it>
2022-08-21 19:54:02 +0200

    UPDATE: hard code users data

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,24 +1,124 @@
 <script>
 	import { onMount } from 'svelte'
-	import { users, activeUser, d3Scale } from '$lib/store'
-	import { getUsers } from '$lib/data'
-	// import { zoom, zoomTransform } from 'd3-zoom'
 	import { select } from 'd3-selection'
+	// import { zoom, zoomTransform } from 'd3-zoom'
+	import { USER_CARD_SIZE } from '$lib/constants'
+	import { activeUser, d3Scale } from '$lib/store'
 
 	import User from '$lib/components/User.svelte'
-	import SearchBar from '$lib/components/SearchBar.svelte'
+	// import SearchBar from '$lib/components/SearchBar.svelte'
 	import Loading from '$lib/components/Loading.svelte'
 
 	let isLoading = true
 	let zoom
 	let zoomTransform
 
+	const users = [
+	{
+		'id': 1,
+		'name': 'Mathi',
+		'birthDate': '2022-02-12T00:00:00Z',
+		'deathDate': '2022-02-12T00:00:00Z',
+		'marriageDate': '2022-02-12T00:00:00Z',
+		'location': 'Italy, Rapallo, Genova',
+		'parents': [],
+		'children': [3],
+		'partner': 2,
+		'avatar': 'https://randomuser.me/api/portraits/men/1.jpg',
+		'x': (USER_CARD_SIZE.width + 10) * 0,
+		'y': (USER_CARD_SIZE.height + 10) * 0
+	},
+	{
+		'id': 2,
+		'name': 'Jeeva',
+		'birthDate': '2022-02-12T00:00:00Z',
+		'deathDate': '2022-02-12T00:00:00Z',
+		'marriageDate': '2022-02-12T00:00:00Z',
+		'location': 'Italy, Rapallo, Genova',
+		'parents': [],
+		'children': [3],
+		'partner': 1,
+		'avatar': 'https://randomuser.me/api/portraits/women/2.jpg',
+		'x': (USER_CARD_SIZE.width + 10) * 1,
+		'y': (USER_CARD_SIZE.height + 10) * 0
+	},
+	{
+		'id': 3,
+		'name': 'Arun',
+		'birthDate': '2022-02-12T00:00:00Z',
+		'deathDate': '2022-02-12T00:00:00Z',
+		'marriageDate': '2022-02-12T00:00:00Z',
+		'location': 'Italy, Rapallo, Genova',
+		'parents': [1, 2],
+		'children': [],
+		'partner': null,
+		'avatar': 'https://randomuser.me/api/portraits/men/3.jpg',
+		'x': (USER_CARD_SIZE.width + 10) * 2,
+		'y': (USER_CARD_SIZE.height + 10) * 0
+	},
+	{
+		'id': 4,
+		'name': 'Raj',
+		'birthDate': '2022-02-12T00:00:00Z',
+		'deathDate': '2022-02-12T00:00:00Z',
+		'marriageDate': '2022-02-12T00:00:00Z',
+		'location': 'Cananda, Toronto, Ontario',
+		'parents': [],
+		'children': [6, 7],
+		'partner': 5,
+		'avatar': 'https://randomuser.me/api/portraits/men/4.jpg',
+		'x': (USER_CARD_SIZE.width + 10) * 0,
+		'y': (USER_CARD_SIZE.height + 10) * 1
+	},
+	{
+		'id': 5,
+		'name': 'Vara',
+		'birthDate': '2022-02-12T00:00:00Z',
+		'deathDate': '2022-02-12T00:00:00Z',
+		'marriageDate': '2022-02-12T00:00:00Z',
+		'location': 'Cananda, Toronto, Ontario',
+		'parents': [],
+		'children': [6, 7],
+		'partner': 5,
+		'avatar': 'https://randomuser.me/api/portraits/women/5.jpg',
+		'x': (USER_CARD_SIZE.width + 10) * 1,
+		'y': (USER_CARD_SIZE.height + 10) * 1
+	},
+	{
+		'id': 6,
+		'name': 'Jana',
+		'birthDate': '2022-02-12T00:00:00Z',
+		'deathDate': '2022-02-12T00:00:00Z',
+		'marriageDate': '2022-02-12T00:00:00Z',
+		'location': 'Cananda, Toronto, Ontario',
+		'parents': [4, 5],
+		'children': [],
+		'partner': null,
+		'avatar': 'https://randomuser.me/api/portraits/men/6.jpg',
+		'x': (USER_CARD_SIZE.width + 10) * 2,
+		'y': (USER_CARD_SIZE.height + 10) * 1
+	},
+	{
+		'id': 7,
+		'name': 'Madhu',
+		'birthDate': '2022-02-12T00:00:00Z',
+		'deathDate': '2022-02-12T00:00:00Z',
+		'marriageDate': '2022-02-12T00:00:00Z',
+		'location': 'Srilanka, Toronto, Ontario',
+		'parents': [4, 5],
+		'children': [],
+		'partner': null,
+		'avatar': 'https://randomuser.me/api/portraits/men/7.jpg',
+		'x': (USER_CARD_SIZE.width + 10) * 3,
+		'y': (USER_CARD_SIZE.height + 10) * 1
+	}
+]
+
 	const sleep = (ms) => new Promise((f) => setTimeout(f, ms))
 
 	onMount(async () => {
 		await sleep(2000) // simulate network delay
 		isLoading = false
-		$users = await getUsers()
 		let d3Zoom = await import('d3-zoom')
 		zoom = d3Zoom.zoom
 		zoomTransform = d3Zoom.zoomTransform
@@ -52,18 +152,18 @@
 {:else}
 	<!-- This is the container that holds GraphView and we have disabled right click functionality to prevent a sticking behavior -->
 	<div
-		id="Nodes"
-		class="absolute h-full min-h-[10000px] w-full min-w-[10000px] cursor-move"
+		id='Nodes'
+		class='absolute h-full w-full cursor-move'
 		on:contextmenu|preventDefault
 		on:click={clearActiveUser}
 	>
 		<!-- This container is transformed by d3zoom -->
-		<div id="Node">
-			{#each $users as user}
+		<div id='Node'>
+			{#each users as user}
 				<User {user} isActive={user.name?.toLowerCase().includes(activeSearch.trim())} />
 			{/each}
 		</div>
 	</div>
 
-	<SearchBar bind:value={activeSearch} />
+	<!-- <SearchBar bind:value={activeSearch} /> -->
 {/if}
