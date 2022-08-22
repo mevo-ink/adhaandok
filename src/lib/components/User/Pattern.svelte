9d64d43f59384d69c037c78b472f91494805c007
Arun Mathiyalakan <friday99@outlook.it>
2022-08-22 11:27:01 +0200

    ADD: pattern color based on relation, but buggy

--- src/lib/components/User/Pattern.svelte
+++ src/lib/components/User/Pattern.svelte
@@ -20,7 +20,6 @@
 
   let color
 	$: color = $activeUser?.name ? ($colorScheme[user.id] ? $colorScheme[user.id] : 'inactive') : 'default'
-  $: console.log(user.name, ' :', color)
 </script>
 
 <svg class='absolute -z-10 w-36 -top-3' height='100' viewBox='0 0 210 100' fill='none'>
@@ -28,10 +27,10 @@
   <path d='M138.574 153L32.4468 46L-49 128.116' stroke='url(#paint1_linear_401_2)' stroke-opacity='0.3' stroke-width='30'/>
   <defs>
     <linearGradient id='paint0_linear_401_2' x1='102.823' y1='-21' x2='102.999' y2='86.0009'>
-      <stop stop-color={colors[`${color}-pattern-primary`]} />
+      <stop class={colors[`${color}-pattern-primary`]} />
     </linearGradient>
     <linearGradient id='paint1_linear_401_2' x1='96.7513' y1='153' x2='96.575' y2='45.9991'>
-      <stop stop-color={colors[`${color}-pattern-secondary`]} />
+      <stop class={colors[`${color}-pattern-secondary`]} />
     </linearGradient>
   </defs>
 </svg>
