2cc0014f44c991d3202092bdbe32e9cb0b172346
Arun Mathiyalakan <friday99@outlook.it>
2022-08-22 11:23:00 +0200

    ADD: scale in & out animation

--- src/lib/components/User.svelte
+++ src/lib/components/User.svelte
@@ -1,8 +1,8 @@
 <script>
 	export let user
 
-	import { activeUser, colorScheme } from '$lib/store'
 	import { USER_CARD_SIZE } from '$lib/constants'
+	import { activeUser, colorScheme } from '$lib/store'
 	import dateFormatter from '$lib/utils/dateFormatter'
 
 	import Pattern from '$lib/components/User/Pattern.svelte'
@@ -62,6 +62,9 @@
 		bg-bottom
 		rounded-md
 		overflow-hidden
+		{color === 'inactive' && 'scale-75'}
+		ease-in-out
+		duration-500
 	'
 	style='
 		left: {user.x}px;
@@ -69,7 +72,6 @@
 		width: {USER_CARD_SIZE.width}px;
 		height: {USER_CARD_SIZE.height}px;
 	'
-	class:animate-shakeY={$activeUser?.id === user.id}
 >
 	<img src={user.avatar} alt={user.name} class='w-full h-full row-span-2 rounded-md drop-shadow-lg' />
 	<div
@@ -87,13 +89,14 @@
 			{`to-${color}-banner-secondary`}
 			text-center
 			overflow-hidden
+			transition
 		'
 		style='text-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25)'
 	>
-		<h1 class='text-title mt-1.5'> {user.name} </h1>
+		<h1 class='text-title mt-2'> {user.name} </h1>
 		<h1 class='text-sub font-light'> {dateFormatter(user.birthDate)} </h1>
 		<h1 class='text-title mt-1.5'> {user.location.substring(0, user.location.indexOf(','))} </h1>
 		<h1 class='text-sub font-light'> {user.location.substring(user.location.indexOf(',') + 1, user.location.length)} </h1>
-		<Pattern />
+		<Pattern {user} />
 	</div>
 </button>
--- src/lib/components/User/Pattern.svelte
+++ src/lib/components/User/Pattern.svelte
@@ -1,12 +1,37 @@
+<script>
+  export let user
+
+  import { activeUser, colorScheme } from '$lib/store'
+
+  const colors = {
+    'default-pattern-primary': '#2DD2D2',
+    'default-pattern-secondary': '#4CC3FF',
+    'inactive-pattern-primary': '#4D4D4D',
+    'inactive-pattern-secondary': '#747474',
+    'partner-pattern-primary': '#AF6CE0',
+    'partner-pattern-secondary': '#F799FF',
+    'parent-pattern-primary': '#C1E89A',
+    'parent-pattern-secondary': '#048D01',
+    'child-pattern-primary': '#63169F',
+    'child-pattern-secondary': '#68008C',
+    'sibling-pattern-primary': '#9F6816',
+    'sibling-pattern-secondary': '#898C00'
+  }
+
+  let color
+	$: color = $activeUser?.name ? ($colorScheme[user.id] ? $colorScheme[user.id] : 'inactive') : 'default'
+  $: console.log(user.name, ' :', color)
+</script>
+
 <svg class='absolute -z-10 w-36 -top-3' height='100' viewBox='0 0 210 100' fill='none'>
   <path d='M61 -21L167.128 86L248.574 3.88372' stroke='url(#paint0_linear_401_2)' stroke-opacity='0.3' stroke-width='30'/>
   <path d='M138.574 153L32.4468 46L-49 128.116' stroke='url(#paint1_linear_401_2)' stroke-opacity='0.3' stroke-width='30'/>
   <defs>
     <linearGradient id='paint0_linear_401_2' x1='102.823' y1='-21' x2='102.999' y2='86.0009'>
-      <stop stop-color='#4AB5FE'/>
+      <stop stop-color={colors[`${color}-pattern-primary`]} />
     </linearGradient>
     <linearGradient id='paint1_linear_401_2' x1='96.7513' y1='153' x2='96.575' y2='45.9991'>
-      <stop stop-color='#4AB5FE'/>
+      <stop stop-color={colors[`${color}-pattern-secondary`]} />
     </linearGradient>
   </defs>
 </svg>
